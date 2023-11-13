d9eedbf8ebd53421ba51346883a2d9abd95609a7
Julius Østerbye <julius310502@gmail.com>
2023-11-13 19:01:01 +0100

    felt navne strings i 2D array

--- Monopoly.java
+++ Monopoly.java
@@ -22,6 +22,30 @@ abstract class Monopoly{
         return this.feltname;
     }
 
+    public static void main(String[] args) {
+
+        String[][] felter = new String[1][16];
+
+        felter[0][0] = "Odense Boldklub";
+        felter[0][1] = "AGF Fodbold";
+        felter[0][2] = "Lyon FC";
+        felter[0][3] = "Toulouse FC";
+        felter[0][4] = "VFL Wolfsburg";
+        felter[0][5] = "Eintracht Frankfurt";
+        felter[0][6] = "Brøndby IF";
+        felter[0][7] = "FC København";
+        felter[0][8] = "Manchester United FC";
+        felter[0][9] = "Chelsea FC";
+        felter[0][10] = "AC Milan";
+        felter[0][11] = "Juventus FC";
+        felter[0][12] = "Tottenham Hotspur FC";
+        felter[0][13] = "Arsenal FC";
+        felter[0][14] = "FC Bayern Munich";
+        felter[0][15] = "Real Madrid CF";
+
+    }
+
+
     public String getDescription(){
         return "Du har slået: " + this.DiceSum + " og er landet på feltet: " + this.feltname + " Din saldo er nu: " + this.saldo;
     }
