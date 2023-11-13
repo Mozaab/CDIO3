405bd54a538fe55c10877d9c832bf58429dabcb7
Mo Zaabat <mzaabat@hotmail.com>
2023-11-13 15:10:57 +0100

    første implem.

---
+++ Monopoly.java
@@ -0,0 +1,16 @@
+abstract class Monopoly{
+
+    public String name;
+    public int saldo;
+    public String felt;
+    
+
+    public Monopoly(String name , int saldo , String felt ){
+
+        this.name = name;
+        this.saldo = saldo;
+        this.felt = felt;
+    }
+
+    
+}
